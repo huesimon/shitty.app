25d7b502b12763a17bcf4f55500819daee8e865f
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2022-01-27 17:35:30 +0100

    Any additional column modifiers must be called before the constrained method:

--- database/migrations/2022_01_27_155325_create_posts_table.php
+++ database/migrations/2022_01_27_155325_create_posts_table.php
@@ -17,7 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->foreignId('user_id')->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->nullOnDelete()->constrained();
             $table->timestamps();
         });
     }
